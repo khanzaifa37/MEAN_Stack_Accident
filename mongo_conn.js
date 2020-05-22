cf58a0ccf6dac8c3152ce20f74883afd10a42f5c
Zaifa Khan <khanzaifa37@gmail.com>
2020-05-22 18:27:17 +0530

    Deploy API

---
+++ mongo_conn.js
@@ -0,0 +1,24 @@
+
+ /*********Get Cloud Foundry credentials to connect mongodb*************/
+
+var MongoClient = require('mongodb').MongoClient;
+let url;
+var credentials = vcapServices.getCredentials('mlab');
+url=credentials.uri;
+ //If Cloud Foundry credentials are not available then ***********//
+ //get local DOCKER running mongo server credintials ************//
+ if (url==null)
+
+url="mongodb://mongo:27017/mynewdb";
+
+ // Connect to the db
+ MongoClient.connect(url, function (err, db) {
+
+if (!err)
+
+console.log("We are connected",url);
+
+else
+
+console.log("Not Connected",url)
+});
